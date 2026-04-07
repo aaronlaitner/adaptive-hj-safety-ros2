a09c2ba4260d613dc58918bb705077678a715126
Edmond <duong.nghuu2003@gmail.com>
2026-04-07 18:57:14 +0000

    Setup for IRL RL

--- setup.py
+++ setup.py
@@ -6,18 +6,19 @@ package_name = 'safe_rl_py'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join("share", package_name, "maps"), glob("maps/*")),
-        # Copies all files in the 'launch' folder
         (os.path.join('share', package_name, 'launch'), glob('launch/*')),
 
         # Copies all files in the 'worlds' folder
         (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+        (os.path.join("share", package_name, "rviz"), glob("rviz/*")),
+        (os.path.join("share", package_name, "params"), glob("params/*")),
         #(os.path.join('share', package_name, 'models', 'your_robot'), glob('models/your_robot/*'))
     ],
     install_requires=['setuptools'],
@@ -34,6 +35,7 @@ setup(
     entry_points={
         'console_scripts': [
             "monitor_node = redexp.robots.turtlebot:main",
+            "robot_pose_publisher = robot_pose_publisher.robot_pose_publisher:main",
         ],
     },
 )
